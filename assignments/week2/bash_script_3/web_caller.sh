93effe1e2998109ce44b01dab55aa74ba3ad6aac
Stephen Card <scard@idevops.io>
2022-01-12 17:54:45 -0500

    added localhost check

--- assignments/week2/bash_script_3/web_caller.sh
+++ assignments/week2/bash_script_3/web_caller.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-#which nginx
+#check if installed first
 echo "Installing nginx"
 sudo apt install nginx
+#check firewall
+sudo ufw allow 'Nginx Full'
+sudo ufw reload
 
 #deploy static page
 echo "installing test html file"
 mkdir -pv ~/www
+chmod 0755 ~/www
+echo "This is the dummy index page." > ~/www/index.html
+
+#check localhost
+echo "Checking local host"
+curl localhost
