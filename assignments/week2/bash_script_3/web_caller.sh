ba4e1eadac7056e12973b1b830f024021ad136f5
Stephen Card <scard@idevops.io>
2022-01-12 18:10:07 -0500

    added configure nginx section

--- assignments/week2/bash_script_3/web_caller.sh
+++ assignments/week2/bash_script_3/web_caller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #check if installed first
 echo "Installing nginx"
-sudo apt install nginx
+sudo apt -y install nginx
 #check firewall
 sudo ufw allow 'Nginx Full'
 sudo ufw reload
@@ -11,6 +11,16 @@ echo "installing test html file"
 mkdir -pv ~/www
 chmod 0755 ~/www
 echo "This is the dummy index page." > ~/www/index.html
+
+#configure nginx
+echo "server {
+        listen 80;
+        server_name localhost;
+        root /root/www;
+        index index.html;
+}" > /etc/nginx/sites-available/localhost
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
 
 #check localhost
 echo "Checking local host"
