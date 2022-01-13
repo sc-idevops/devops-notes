adc1b4b7e12fa750e7adf47643772fde75c8b547
Stephen Card <scard@idevops.io>
2022-01-12 19:36:12 -0500

    removed unnecessary line

--- assignments/week2/bash_script_3/nginx_install.sh
+++ assignments/week2/bash_script_3/nginx_install.sh
@@ -6,7 +6,6 @@ sudo apt -y install nginx
 
 #deploy static page
 echo "installing test html file"
-sudo rm /var/www/html/index.html
 echo "Test File" > /var/www/html/index.html
 
 #check test.idevops.com
