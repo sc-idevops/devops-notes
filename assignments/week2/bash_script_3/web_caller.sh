2cf2f6d307b74a203ea5e48874edd27442f3c903
Stephen Card <scard@idevops.io>
2022-01-12 19:29:25 -0500

    simplified

--- assignments/week2/bash_script_3/web_caller.sh
+++ assignments/week2/bash_script_3/web_caller.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 #check if installed first
 echo "Installing nginx"
+sudo apt update
 sudo apt -y install nginx
-#check firewall
-sudo ufw allow 'Nginx Full'
-sudo ufw reload
 
 #deploy static page
 echo "installing test html file"
-mkdir -pv ~/www
-chmod 0755 ~/www
-echo "This is the dummy index page." > ~/www/index.html
+sudo rm /var/www/html/index.html
+echo "Test File" > /var/www/html/index.html
 
-#configure nginx
-echo "server {
-        listen 80;
-        server_name localhost;
-        root /root/www;
-        index index.html;
-}" > /etc/nginx/sites-available/localhost
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
-sudo systemctl restart nginx
-
-#check localhost
+#check test.idevops.com
 echo "Checking local host"
 curl localhost
