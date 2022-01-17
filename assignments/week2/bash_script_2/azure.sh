159fd9c25f54a57040d2b22957295be580423ca7
Stephen Card <scard@idevops.io>
2022-01-17 00:59:50 -0500

    iterate through groups

--- assignments/week2/bash_script_2/azure.sh
+++ assignments/week2/bash_script_2/azure.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-
-#List Subscriptions
-sub=$(az account show --query 'name')
-echo "Subcriptions:" $sub
 #Get List of Group Names and store in $grp
-grp=$(az group list -o tsv)
+grp=$(az group list -o tsv --query "[?name].name")
 
-for g in "${grp[@]}"; do
-  echo $g
-  #az resource list --output table --resource-group $g
+#Iterate through array grp and output resources for that group name
+for g in $grp; do
+  echo -e "\nResources in Resource Group: $g\n"
+  az resource list --output table --resource-group $g
 done
