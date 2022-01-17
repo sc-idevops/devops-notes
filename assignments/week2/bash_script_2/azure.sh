027604dcb9a7511a220536eb534857984540d28c
Stephen Card <scard@idevops.io>
2022-01-17 01:16:59 -0500

    done?

--- assignments/week2/bash_script_2/azure.sh
+++ assignments/week2/bash_script_2/azure.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 #list of subscriptions
-sub=$(az account show --query "name")
+sub=$(az account list -o tsv --query "[].name")
 #Get List of Group Names and store in $grp
-grp=$(az group list -o tsv --query "[?name].name")
+grp=$(az group list -o tsv --query "[].name")
 
-
-#Iterate through array grp and output resources for that group name
-for g in $grp; do
-  echo -e "\nResources in Resource Group: $g\n"
-  az resource list --output table --resource-group $g
+for s in $sub; do
+  echo -e "\nSubscription: $s\n"
+  #Iterate through array grp and output resources for that group name
+  for g in $grp; do
+    echo -e "\nResources in Resource Group: $g\n"
+    az resource list --output table --resource-group $g
+  done
 done
