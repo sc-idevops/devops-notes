78a52dd192db05b3292c07ebd45acae89a0e3ad4
Stephen Card <scard@idevops.io>
2022-01-17 20:34:51 -0500

    refinement: check service status

--- assignments/week2/bash_script_3/nginx_install.sh
+++ assignments/week2/bash_script_3/nginx_install.sh
@@ -4,10 +4,12 @@ echo "Installing nginx"
 sudo apt update
 sudo apt -y install nginx
 
+#check status of nginx
+sudo systemctl status nginx.service || echo "E: Nginx not started correctly!"
 #deploy static page
 echo "installing test html file"
-echo "Test File" > /var/www/html/index.html
+echo "<html><title>Test File</title></html>" > /var/www/html/index.html
 
-#check test.idevops.com
+#check localhost
 echo "Checking local host"
 curl localhost
