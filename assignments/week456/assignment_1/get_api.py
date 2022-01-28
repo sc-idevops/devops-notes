c00fb0bf23928bca4a3ff1690c300bf95e4d7d75
stephen card <scard@idevops.io>
2022-01-28 04:51:38 -0500

    started assignment

---
+++ assignments/week456/assignment_1/get_api.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#reference https://docs.python-requests.org/en/latest/user/quickstart/
+import requests
+
+r = requests.git('https://jsonplaceholder.typicode.com/posts')
