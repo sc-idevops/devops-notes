f209c7ac1dbccb31e8f7279e12c7b20f59c7e5b9
stephen card <scard@idevops.io>
2022-01-28 18:50:21 -0500

    closer

--- assignments/week456/assignment_2/get_website.py
+++ assignments/week456/assignment_2/get_website.py
@@ -5,9 +5,11 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://store.steampowered.com/app/1343370/Old_School_RuneScape/"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
-#  print(soup)
-div = soup.find_all("div", "game_purchase_price")
-
+f = open("output.txt", "w")
+f.write(r.text)
+f.close
+div = soup.find_all("div", class_="game_purchase_price")
+print(div[0])
