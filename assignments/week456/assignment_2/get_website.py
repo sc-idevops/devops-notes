bcfc89bd8374b5b587d50a801738f6960ec49358
stephen card <scard@idevops.io>
2022-01-28 20:52:31 -0500

    finished assignment 2

--- assignments/week456/assignment_2/get_website.py
+++ assignments/week456/assignment_2/get_website.py
@@ -8,8 +8,7 @@ url = "https://store.steampowered.com/app/1343370/Old_School_RuneScape/"
 #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
-f = open("output.txt", "w")
-f.write(r.text)
-f.close
+
 div = soup.find_all("div", class_="game_purchase_price")
-print(div[0])
+print(div[0].text.strip())
+#text.strip() is a function of print which can be used to clean up a result
