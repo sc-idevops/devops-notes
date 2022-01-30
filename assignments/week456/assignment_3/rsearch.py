c40ebf23efe6beb73293c8b6e56db9d73ce144b9
stephen card <scard@idevops.io>
2022-01-30 01:31:37 -0500

    finished assignment 3

--- assignments/week456/assignment_3/rsearch.py
+++ assignments/week456/assignment_3/rsearch.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+os.chdir('/usr/bin')
+path = os.getcwd()
+name = "bash"
+print("Current Directory: ", os.getcwd())
+
+for root, dirs, files in os.walk(path):
+    if name in files:
+        print(os.path.join(root,name))
