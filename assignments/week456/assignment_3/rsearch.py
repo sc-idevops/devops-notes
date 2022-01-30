8131047d6b7b74ca19baafcab04436449e7505b4
stephen card <scard@idevops.io>
2022-01-30 01:35:21 -0500

    refinement

--- assignments/week456/assignment_3/rsearch.py
+++ assignments/week456/assignment_3/rsearch.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-os.chdir('/usr/bin')
+os.chdir('/usr/')
 path = os.getcwd()
 name = "bash"
-print("Current Directory: ", os.getcwd())
+print(">> Current Directory: ", os.getcwd())
+print(">> Scanning for: ", name)
 
 for root, dirs, files in os.walk(path):
     if name in files:
