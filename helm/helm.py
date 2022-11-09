5058c5cbf348758e6322e7dc5b12df280342d45c
Stephen Card <scard@idevops.io>
2022-11-08 22:26:11 -0500

    reconfigure as a load balancer

--- helm/helm.py
+++ helm/helm.py
@@ -1,5 +1,5 @@
 import redis
-r = redis.Redis(password="WRtRpAcFZq")
+r = redis.Redis(password="4jbZpRULOR",host="a0d695fd9be1245d29e24249a92561a3-1895269611.us-west-2.elb.amazonaws.com")
 r.mset({"iDevOps": "Grant Is The Man"})
 retrieve_value = r.get("iDevOps")
 print(retrieve_value.decode("utf-8"))
